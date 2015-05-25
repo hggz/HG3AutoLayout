2986e0ab6f91d364878e16dcb8e954c013d3152a
Hugo Artemio Gonzalez <hagonzalez94@gmail.com>
2015-05-24 18:07:31 -0700

    added AutoLayout View Extension

---
+++ HG3AutoLayout/HG3ViewExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  HG3ViewExtensions.swift
+//  HG3AutoLayout
+//
+//  Created by Hugo Gonzalez on 5/24/15.
+//  Copyright (c) 2015 HG3. All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+    func newAutoLayoutObject() -> UIView {
+        var view = UIView()
+        view.setTranslatesAutoresizingMaskIntoConstraints(false)
+        return view
+    }
+    
+    func updateLayoutForView(view: UIView) {
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+    }
+}
