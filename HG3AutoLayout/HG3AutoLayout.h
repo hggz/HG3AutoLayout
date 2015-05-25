5b948b542259a5c51f6a20dd0376fbe675daba34
Hugo Artemio Gonzalez <hagonzalez94@gmail.com>
2015-05-24 17:08:56 -0700

    Initial Commit

---
+++ HG3AutoLayout/HG3AutoLayout.h
@@ -0,0 +1,19 @@
+//
+//  HG3AutoLayout.h
+//  HG3AutoLayout
+//
+//  Created by Hugo Gonzalez on 5/24/15.
+//  Copyright (c) 2015 HG3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HG3AutoLayout.
+FOUNDATION_EXPORT double HG3AutoLayoutVersionNumber;
+
+//! Project version string for HG3AutoLayout.
+FOUNDATION_EXPORT const unsigned char HG3AutoLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HG3AutoLayout/PublicHeader.h>
+
+
